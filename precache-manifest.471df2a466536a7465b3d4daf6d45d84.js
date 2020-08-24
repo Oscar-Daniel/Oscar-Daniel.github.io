906185a431531ec2316daaa2a3001a1101cd2909
Oscar Daniel <t0d0c4m614@hotmail.com>
2020-08-23 21:14:50 -0500

    Updates

---
+++ precache-manifest.471df2a466536a7465b3d4daf6d45d84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e31c5995cf5ce7ffc2ae34bd089e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da2a45eea65b4d251f5c",
+    "url": "/static/css/main.42ff3e17.chunk.css"
+  },
+  {
+    "revision": "cff1834de23155cb5e25",
+    "url": "/static/js/2.835bae97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.835bae97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2a45eea65b4d251f5c",
+    "url": "/static/js/main.9cb00c91.chunk.js"
+  },
+  {
+    "revision": "ecf937347709dd1bf9f7",
+    "url": "/static/js/runtime-main.3d80e7a1.js"
+  },
+  {
+    "revision": "699bef077083fc79e9ab6b75d52581d8",
+    "url": "/static/media/profile.699bef07.jpg"
+  }
+]);
